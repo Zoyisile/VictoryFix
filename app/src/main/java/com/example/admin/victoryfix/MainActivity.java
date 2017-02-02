9db1f869d36cdd0f962af34d2586aa9373408944
admin <admin>
2017-02-02 14:57:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/admin/victoryfix/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.victoryfix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
